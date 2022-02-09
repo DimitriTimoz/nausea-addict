ed15ac03a15b55af7aca98b1970d123ed5a9238f
DimitriTimoz <45998396+DimitriTimoz@users.noreply.github.com>
2022-02-10 00:49:45 +0100

    move addEventListener

--- src/main.js
+++ src/main.js
@@ -3,49 +3,42 @@ let poison_nausea=`
 <style>
 body {
     animation:spin 3s linear infinite;
-    overflow: hidden;
+    overflow: hidden !important;
 }
-
-
-
 @keyframes spin 
 { 
     0% {         
-        -webkit-transform: rotate(7200deg) scale(0.0001); 
+        -webkit-transform: rotate(0deg) scale(0.0001); 
     }
     50%{
-        -webkit-transform: rotate(3600deg) scale(2); 
-        transform:rotate(3600deg) scale(1); 
+        -webkit-transform: rotate(3600deg) scale(1.5); 
+        transform:rotate(3600deg) scale(1.5); 
     }
     100% { 
         -webkit-transform: rotate(7200deg) scale(0.0001); 
         transform:rotate(7200deg) scale(0.0000001); 
         filter: hue-rotate(360deg);
-        
-        
-        
-    } 
+   } 
 }
 </style>
 `;
-var delayInMilliseconds = 1500; //1.5 seconds
 
-window.addEventListener("load", setTimeout(() => {
-    
-    chrome.storage.local.get(["blacklist"], (items) => {
-        let blacklist = items.blacklist;
-        if(items.blacklist == null){
+const delayInMilliseconds = 1500;
+
+chrome.storage.local.get(["blacklist"], (items) => {
+    if(items.blacklist == null){
+        return;
+    }
+    let blacklist = items.blacklist;
+    let labels = window.location.host.split('.');
+    let domain = labels[labels.length - 2]; 
+    blacklist.forEach(blackhost => {
+        if(blackhost == domain){
+            window.addEventListener("load", setTimeout(() => {
+                document.body.innerHTML += poison_nausea;
+            }, delayInMilliseconds));
             return;
         }
-        let labels = window.location.host.split('.');
-        let domain = labels[labels.length - 2]; 
-        blacklist.forEach(blackhost => {
-
-            if(blackhost == domain){
-                document.body.innerHTML += poison_nausea;
-                return;
-            }
-        });
-        
     });
-}, delayInMilliseconds));
+    
+});
